745cff9f84fa1930cd2eca655aef4e9be87480b2
アカメ <mooraesz123@gmail.com>
2025-10-20 12:34:59 -0300

    feat: implementing helper binaries

---
+++ lib/src/bin/block_print.rs
@@ -0,0 +1,15 @@
+use std::env;
+
+fn main() {
+    let path = if let Some(arg) = env::args().nth(1) {
+        arg
+    } else {
+        eprintln!("Usage: block_print <block_file>");
+        exit(1);
+    };
+
+    if let Ok(file) = File::open(path) {
+        let block = Block::load(file).expect("Failed to load block");
+        println!("{:#?}", block);
+    }
+}
